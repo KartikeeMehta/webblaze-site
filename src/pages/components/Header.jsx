ded2467150dfce7cb2fe091eeda271357acda2a0
Nisha <devx@Nishas-MacBook-Air.local>
2025-06-10 17:25:22 +0530

    add section b

--- src/pages/components/Header.jsx
+++ src/pages/components/Header.jsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 function Header() {
   return (
     <header className="bg-white shadow-md sticky top-0 z-50">
-      <div className="flex items-center justify-between max-w-[1200px] m-auto px-5">
-        <div className="w-40 md:w-48">
+      <div className="flex items-center justify-between max-w-[1200px] m-auto p-5">
+        <div className="w-[135px] h-[36px]">
           <img
             src="https://webblazesofttech.com/wp-content/uploads/2024/07/webblaze-socttech-logo.svg"
             alt="Blaze Softtech Logo"
@@ -13,7 +13,7 @@ function Header() {
           />
         </div>
 
-        <nav className="hidden lg:flex space-x-6 text-gray-800 font-medium">
+        <nav className="hidden lg:flex gap-[15px] text-gray-800 text-sm font-medium">
           <Link to="/">Services</Link>
           <Link to="/">Our Solutions</Link>
           <Link to="/">Digital Marketing</Link>
@@ -25,10 +25,10 @@ function Header() {
         </nav>
 
         <div className="hidden lg:flex space-x-3">
-          <button className="bg-blue-500 text-white px-4 py-2 rounded-full hover:bg-blue-600 transition duration-200">
+          <button className="bg-blue-500 text-white text-sm  rounded-[12px] hover:bg-blue-600 transition duration-200 py-[10px] px-[20px]">
             Get in Touch
           </button>
-          <button className="bg-blue-500 text-white px-4 py-2 rounded-full hover:bg-blue-600 transition duration-200">
+          <button className="bg-blue-500 text-white text-sm  rounded-[12px] hover:bg-blue-600 transition duration-200 py-[10px] px-[20px]">
             Free SEO Audit
           </button>
         </div>

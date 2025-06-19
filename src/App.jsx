083b99ada591bd324ea990c3a9a6945c6ed152ee
Nisha <devx@Nishas-MacBook-Air.local>
2025-06-19 12:09:24 +0530

    merging

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ import Shopify from "./pages/Shopify";
 import Shopify_app from "./pages/Shopify_app";
 import Php_laravel from "./pages/Php_laravel";
 import Javascript from "./pages/Javascript";
-import ReactJs from "./pages/ReactJs";
 import VueJs from "./pages/VueJs";
 
 function App() {
@@ -21,7 +20,7 @@ function App() {
         <Route path="/Uiux_app" element={<Uiux_app />} />
         <Route path="/Word_Press" element={<Word_Press />} />
         <Route path="/Web_Design" element={<Web_Design />} />
-      <Route path="/Shopify" element={<Shopify />} />
+        <Route path="/Shopify" element={<Shopify />} />
         <Route path="/Shopify_app" element={<Shopify_app />} />
         <Route path="/Php_laravel" element={<Php_laravel />} />
         <Route path="/Javascript" element={<Javascript />} />
